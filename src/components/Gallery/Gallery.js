79f4c58cd16e424cac421c4321d212f20e61537e
DESKTOP-OP0FSSP\anast <anastasiu.tudor@gmail.com>
2021-12-03 21:30:31 +0200

    gallery loading screen

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,11 +1,12 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect,useContext } from "react";
 import axios from "axios";
 import url from "../../utilis/URL";
 import GalleryElem from "./GalleryElem";
 import { flattenProducts } from "../../utilis/helpers";
+import {ProductContext} from '../../context/products'
 const Gallery = () => {
   const [gallery, setGallery] = useState([]);
-
+  const {loading} = useContext(ProductContext)
   useEffect(() => {
     axios.get(`${url}/galleries`).then((response) => {
       const gallery = flattenProducts(response.data);
@@ -14,7 +15,11 @@ const Gallery = () => {
 
     return () => {};
   }, []);
-
+  if(loading) {
+    return <section className="loading-gallery">
+      <h2 className="loading-text">Loading...</h2>
+    </section>
+  }
   return (
     <section className="gallery">
       <h1>Our Gallery</h1>
